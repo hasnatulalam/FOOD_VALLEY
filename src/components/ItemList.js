eb524b261ae4be5b89b1711bd0e15ecd998de6c2
hasnatulalam <hasnatualam056@gmail.com>
2023-11-16 16:03:30 +0600

    img added

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -5,14 +5,18 @@ const ItemList =({items})=>{
         <div >
           {items.map((item)=>(
            
-            <div className="card w-6/12 mx-auto h-40 justify-between items-center bg-blue-dark text-gray  px-10" key={item.card.info.id} >
+            <div className="flex card w-6/12 mx-auto h-40 justify-between  items-center bg-blue-dark text-gray  px-10" key={item.card.info.id} >
             <div>
                 <h3 className="font-bold">{item.card.info.name}</h3>
                 <h3>{item.card.info.price}</h3>
             </div>
-               <div>
-               
-               </div>
+        <div>
+               <img
+          className="w-[118px] h-[96px]"
+          src={ITEM_IMG_CDN + item.card.info.imageId}
+          alt={item?.name}
+        />  
+        </div>
                 
           </div>
           ))}
